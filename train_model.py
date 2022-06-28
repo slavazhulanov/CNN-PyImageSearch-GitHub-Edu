d677de47bf7531f3b70d52a5a35b9dc875284e04
Slava Zhulanov <590slavv@gmail.com>
2022-06-28 12:02:03 +0500

    train model

---
+++ train_model.py
@@ -0,0 +1,8 @@
+# import the necessary packages
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+import argparse
+import pickle
+import h5py
+
