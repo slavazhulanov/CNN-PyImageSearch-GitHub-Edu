3d949b839f535c6d408a8d73d5e438368a0fe666
Жуланов Вячеслав <zhulanov@domination.one>
2022-10-12 10:04:03 +0500

    10.3 Implementing AlexNet

--- pyimagesearch/dogs_vs_cats/config/dogs_vs_cats_config.py
+++ pyimagesearch/dogs_vs_cats/config/dogs_vs_cats_config.py
@@ -1,5 +1,5 @@
 # define the paths to the images directory
-IMAGES_PATH = "C:/PycharmProjects/CNN-PyImageSearch-GitHub-Edu/pyimagesearch/datasets/kaggle_dogs_vs_cats/train"
+IMAGES_PATH = "D:/PycharmProjects/CNN-PyImageSearch-GitHub-Edu/pyimagesearch/datasets/kaggle_dogs_vs_cats/train"
 
 # since we do not have validation data or access to the testing
 # labels we need to take a number of images from the training
@@ -10,16 +10,16 @@ NUM_TEST_IMAGES = 0.2
 
 # define the path to the output training, validation, and testing
 # HDF5 files
-TRAIN_HDF5 = "C://PycharmProjects//CNN-PyImageSearch-GitHub-Edu//pyimagesearch//datasets//kaggle_dogs_vs_cats//hdf5//train.hdf5"
-VAL_HDF5 = "C:/PycharmProjects/CNN-PyImageSearch-GitHub-Edu/pyimagesearch/datasets/kaggle_dogs_vs_cats/hdf5/val.hdf5"
-TEST_HDF5 = "C:/PycharmProjects/CNN-PyImageSearch-GitHub-Edu/pyimagesearch/datasets/kaggle_dogs_vs_cats/hdf5/test.hdf5"
+TRAIN_HDF5 = "D://PycharmProjects//CNN-PyImageSearch-GitHub-Edu//pyimagesearch//datasets//kaggle_dogs_vs_cats//hdf5//train.hdf5"
+VAL_HDF5 = "D:/PycharmProjects/CNN-PyImageSearch-GitHub-Edu/pyimagesearch/datasets/kaggle_dogs_vs_cats/hdf5/val.hdf5"
+TEST_HDF5 = "D:/PycharmProjects/CNN-PyImageSearch-GitHub-Edu/pyimagesearch/datasets/kaggle_dogs_vs_cats/hdf5/test.hdf5"
 
 # path to the output model file
-MODEL_PATH = "C:\PycharmProjects\CNN-PyImageSearch-GitHub-Edu\pyimagesearch/dogs_vs_cats/output/alexnet_dogs_vs_cats.model"
+MODEL_PATH = "D:\PycharmProjects\CNN-PyImageSearch-GitHub-Edu\pyimagesearch/dogs_vs_cats/output/alexnet_dogs_vs_cats.model"
 
 # define the path to the dataset mean
-DATASET_MEAN = "C:\PycharmProjects\CNN-PyImageSearch-GitHub-Edu\pyimagesearch/dogs_vs_cats/output/dogs_vs_cats_mean.json"
+DATASET_MEAN = "D:\PycharmProjects\CNN-PyImageSearch-GitHub-Edu\pyimagesearch/dogs_vs_cats/output/dogs_vs_cats_mean.json"
 
 # define the path to the output directory used for storing plots,
 # classification reports, etc.
-OUTPUT_PATH = "C:\PycharmProjects\CNN-PyImageSearch-GitHub-Edu\pyimagesearch/dogs_vs_cats/output"
+OUTPUT_PATH = "D:\PycharmProjects\CNN-PyImageSearch-GitHub-Edu\pyimagesearch/dogs_vs_cats/output"
